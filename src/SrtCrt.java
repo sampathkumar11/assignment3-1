7bc03cb47afbe530c9bc4a78fd149cf367a05eec
sampathkumar11 <sampathkumar11@gmail.com>
2016-02-06 15:36:02 +0530

    Session 3 -1
    Finished

--- src/SrtCrt.java
+++ src/SrtCrt.java
@@ -1,18 +1,19 @@
+import java.util.Scanner;
 
 public class SrtCrt {
 	String s;
 	String c;	
 
 
-	static String srt(String s){
-		double sd=Double.parseDouble(s);
+	static String srt(Double sd){
+		//double sd=Double.parseDouble(s);
 		double srt_val=Math.sqrt(sd);
 		String op="Square root of "+sd+" is "+srt_val;
 		return op;
 	}
 
-	static String crt(String c){
-		double cd=Double.parseDouble(c);
+	static String crt(Double cd){
+		//double cd=Double.parseDouble(c);
 		double crt_val=Math.cbrt(cd);
 		String op="Cube root of "+cd+ " is "+crt_val;
 		return op;
@@ -21,32 +22,22 @@ public class SrtCrt {
 
 	public static void main(String[] args)
 	{
-//		System.out.println("Hello World!");
-		SrtCrt obj_sc=new SrtCrt();
+		System.out.println("Enter the number whose square root has to be found :");
+		//SrtCrt obj_sc=new SrtCrt();
+		Scanner scnr = new Scanner(System.in);
 		//obj_sc.s="100.0";
-		String s="100.0";
+		//String s="100.0";
+		Double s=scnr.nextDouble();
 		System.out.println(srt(s));
 		
-		String c="8.0";
+		//String c="8.0";
+		
+		System.out.println("Enter the number whose Cube root has to be found :");
+		Double c=scnr.nextDouble();
+		
 		System.out.println(crt(c));
+		scnr.close();
 		
 	}
 
 }
-
-/*
-class SrtCrt{
-	public static void main(String[] args){
-		
-		double sd=100.0;
-		double srt_val=Math.sqrt(sd);
-		System.out.println("Square root of "+sd+ "is "+srt_val);
-		
-		
-		double cd=625.00;
-		double crt_val=Math.sqrt(cd);
-		System.out.println("Cube root of "+cd+ "is "+crt_val);
-			
-		
-	}
-}*/
